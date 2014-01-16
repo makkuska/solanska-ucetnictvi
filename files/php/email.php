0514946632f2ca4605243b9718726791155dd359
Markéta Solanská <magiess@volny.cz>
2014-01-16 21:57:04 +0100

    oprava validace formuláře i pro IE8

--- files/php/email.php
+++ files/php/email.php
@@ -40,16 +40,16 @@
     </div> <!-- div navigator -->
     <?php
       //seznam mailů, na které se zpráva odesílá
-      $email = 'solanska@solanska.eu,marketa.solanska@gmail.com';
+      $to = 'marketa.solanska@gmail.com';
+      $from = $_POST['odesilatel_mail'];
+      //solanska@solanska.eu,
       //proměnné naplněné z formuláře
-      $zprava = $_POST['zprava'];
-      $predmet = $_POST['predmet'];
-      $odesilatel = $_POST['odesilatel_mail'];
+      $formSubject = $_POST['predmet'];
+      $formMessage = $_POST['zprava'];
+      $formUrl = $_POST['url'];
       
-      $pr = "Dotaz z webu: $predmet";
-
       //hlavicka
-        $hlavicka = "From:$odesilatel\n";
+        $hlavicka = "From:$from\n";
         $hlavicka .= "Subject:$pr\n";
         $hlavicka .= "MIME-Version: 1.0\n";
         // způsob kódování
@@ -58,19 +58,38 @@
         $hlavicka .= "Content-Type: text/plain; charset=UTF-8\n"; // Kódování
   
       // formát zprávy pro solanska@solanska.eu
-      $zp = 'Předmět zprávy: ' . $predmet . "\n" . 'Text zprávy: ' . $zprava;
+      $subjectCopy = 'Kopie zprávy z webu www.solanska.eu';
+      $messageCopy = 'Vaše zpráva, kterou jste odeslali přes web www.solanska.eu:' . "\n" .$formMessage;
       
       //kopie emailu pro odesílatele dotazu
-      $zp_prijemce = 'Vaše zpráva, kterou jste odeslali přes web www.solanska.eu:' . "\n" .$zprava;
-      mail($odesilatel, 'Kopie zprávy z webu www.solanska.eu', $zp_prijemce, $hlavicka);
+      $subjectOrig = "Dotaz z webu: $formSubject";
 
-      //mail na solanska@solanska.eu
-      $vysledek = mail($email, $pr, $zprava, $hlavicka);
 
-      if ($vysledek)
-          echo('<br /><div class="jumbotron"><div class="container"><p>Váš mail byl úspěšně odeslán. Děkujeme za vaši zprávu či dotaz.</><p> Po přečtení se Vám budeme snažit, co nejdříve odpovědět na mail, který jste zadali ' . '('.$odesilatel.').</p><p><a class="btn btn-default" href="http://www.solanska.eu">Zpět na solanska.eu</a></p></div></div>');
-        else
-          echo('<br /><div class="jumbotron"><div class="container"><p>Omlouváme se, někde nastala chyba. Váš e-mail nebyl odeslán. </p><p>Zkuste zadat požadavek znovu, zkontrolujte, že jste vyplnili všechny kolonky. V případě, že to stále nepůjde, kontaktujte nám přímo na e-mailu solanska@solanska.eu nebo na telefonním čísle 724 063 868. Děkujeme za pochopení.</p><p><a class="btn btn-default" href="http://www.solanska.eu">Zpět na solanska.eu</a></p></div></div>');
+      if (!empty($formUrl)) {
+          $page1  = '<br /><div class="jumbotron"><div class="container"><p>' .$formUrl .
+          $page1 .= '</p><a class="btn btn-default" href="http://www.solanska.eu">';
+          $page1 .= ' Zpět na solanska.eu</a></p> </div></div>';
+          echo ($page1);
+      } else if (empty($from) or empty($formMessage) or empty($formSubject)) { 
+          $page2 = '<br /><div class="jumbotron"><div class="container">';
+          $page2 .= '<p>Omlouváme se, někde nastala chyba. Váš e-mail nebyl odeslán.</p>';
+          $page2 .= '<p>Zkuste zadat požadavek znovu, zkontrolujte, že jste vyplnili všechny kolonky.';
+          $page2 .= 'V případě, že to stále nepůjde, kontaktujte nám přímo na e-mailu solanska@solanska.eu ';
+          $page2 .= 'nebo na telefonním čísle 724 063 868. Děkujeme za pochopení.</p><p>';
+          $page2 .= '<a class="btn btn-default" href="http://www.solanska.eu">Zpět na solanska.eu</a></p>';
+          $page2 .= '</div></div>';
+          echo ($page2);
+      } else {
+          mail($to,   $subjectOrig, $formMessage, $hlavicka);
+          mail($from, $subjectCopy, $messageCopy, $hlavicka);
+          $page3 = '<br /><div class="jumbotron"><div class="container">';
+          $page3 .= '<p>Váš mail byl úspěšně odeslán. Děkujeme za vaši zprávu či dotaz.</p>';
+          $page3 .= '<p> Po přečtení se Vám budeme snažit, co nejdříve odpovědět na mail, který jste zadali ('. $to.').</p>';
+          $page3 .= '<p><a class="btn btn-default" href="http://www.solanska.eu">Zpět na solanska.eu</a></p>';
+          $page3 .= '</div></div>';
+          echo ($page3);
+      }
+
     ?>
 </body>
 </html>
