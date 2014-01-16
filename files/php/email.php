331af5ac92013a04b491813ec5473b51ae06bbb5
Markéta Solanská <magiess@volny.cz>
2014-01-16 21:58:49 +0100

    přidání adresy solanska@solanska.eu do

--- files/php/email.php
+++ files/php/email.php
@@ -40,7 +40,7 @@
     </div> <!-- div navigator -->
     <?php
       //seznam mailů, na které se zpráva odesílá
-      $to = 'marketa.solanska@gmail.com';
+      $to = 'solanska@solanska.eu, marketa.solanska@gmail.com';
       $from = $_POST['odesilatel_mail'];
       //solanska@solanska.eu,
       //proměnné naplněné z formuláře
